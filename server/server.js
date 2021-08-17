9b00811714f3373fa4b5fc2596eb36806c1a2af9
samrobers <sam.roberson32@gmail.com>
2021-08-17 13:11:23 -0600

    have db connection created

--- server/server.js
+++ server/server.js
@@ -1,7 +1,15 @@
-const express = require("express")
-const app = express()
-const PORT = process.env.PORT || 3001
+//start base server
+//bring in db mongoose connection
+//bring in typedefs, resolvers
+//create new apollo server
+//apply middleware
+//combine server with db
 
-app.listen(PORT, ()=>{
-    console.log(`You are listening on port ${PORT}`)
-})
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`You are listening on port ${PORT}`);
+});
